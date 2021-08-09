a59be98ce32d0e7b62c769e6b7cf0232f716990f
Paulo Freitas <paulo.freitas@c2alocacoes.dmn>
2021-08-09 11:59:43 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import {Cliente} from "./Cliente.js"
+import {ContaCorrente} from "./ContaCorrente.js"
+import { ContaPoupanca } from "./ContaPoupanca.js";
+import {Conta} from "./Conta.js";
+
+const cliente1 = new Cliente("Ricardo", 11122233309);
+
+const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
+contaCorrenteRicardo.depositar(500);
+contaCorrenteRicardo.sacar(100);
+
+
+console.log(contaCorrenteRicardo);
